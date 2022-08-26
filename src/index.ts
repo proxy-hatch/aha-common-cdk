6021da00d986b580ce2f2b67f442c8f20fc112a9
Shawn Wang <shawn.wang@avancevl.com>
2022-08-26 13:14:50 +0800

    exporting type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
-export interface DeploymentEnvironment {
-  readonly region: string;
-  readonly stage: string;
-  readonly accountId: number;
-  readonly appPrefix: string;
+export type DeploymentEnvironment = {
+  region: string;
+  stage: string;
+  accountId: number;
+  appPrefix: string;
 }
+
+
+//
+// export class Abc implements DeploymentEnvironment {
+//   readonly accountId!: number;
+//   readonly appPrefix!: string;
+//   readonly region!: string;
+//   readonly stage!: string;
+//
+//   Abc(){
+//
+//   }
+// }
