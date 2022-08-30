7f9f215f694203d24f0d18fae41f94d16ea22971
Shawn Wang <shawn.wang@avancevl.com>
2022-08-30 12:03:55 +0800

    feat: add basic helper objects

---
+++ README.md
@@ -0,0 +1,7 @@
+# Aha Common CDK library
+
+TODO: add README to describe how to integrate with new infra pkg
+
+[api-core#1595](https://app.zenhub.com/workspaces/back-edtech-623a878cdf3d780017775a34/issues/earnaha/api-core/1595)
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,40 @@
-export type DeploymentEnvironment = {
-  region: string;
-  stage: string;
-  accountId: number;
-  appPrefix: string;
+import { Environment } from 'aws-cdk-lib';
+
+export const STAGES = {
+  Alpha: 'alpha',
+  Beta: 'beta',
+  Gamma: 'gamma',
+  Prod: 'prod',
+} as const;
+
+export const SERVICES = {
+  ApiCore: 'api-core',
+  // ApiAuth: 'api-auth',
+} as const;
+
+
+export interface DeploymentEnvironment extends Environment {
+  readonly account: string;
+  readonly region: string;
+  readonly stage: string;
+  readonly appPrefix: string;
 }
 
+export interface StackCreationInfo {
+  readonly account: string;
+  readonly region: string;
+  readonly stage?: string;
+  readonly stackPrefix: string;
+}
 
-//
-// export class Abc implements DeploymentEnvironment {
-//   readonly accountId!: number;
-//   readonly appPrefix!: string;
-//   readonly region!: string;
-//   readonly stage!: string;
-//
-//   Abc(){
-//
-//   }
-// }
+export function createStackCreationInfo(account: string, region: string, stage?: string): StackCreationInfo {
+  return {
+    account: account,
+    region: region,
+    stackPrefix: `Aha-${ region }${ stage ? '-' + stage : '' }`,
+  };
+}
+
+export const AHA_DEFAULT_REGION = 'ap-northeast-1';
+
+export const AHA_ORGANIZATION_ACCOUNT = '083784680548';
