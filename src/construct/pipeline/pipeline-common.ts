55b19198ed5c61d306e3f16b417385fd2604197a
Shawn Wang <shawn.wang@avancevl.com>
2022-09-15 15:34:54 +0800

    use local git

--- src/construct/pipeline/pipeline-common.ts
+++ src/construct/pipeline/pipeline-common.ts
@@ -143,8 +143,9 @@ export function buildSynthStep(trackingPackages: TrackingPackage[], service: SER
     },
     commands: [
       'cd cdk',
+      'git init',
       '/usr/bin/git config --list --show-origin --show-scope',
-      '/usr/bin/git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf https://git@github.com/:',
+      '/usr/bin/git config --local url."https://${GITHUB_TOKEN}@github.com/".insteadOf https://git@github.com/:',
       '/usr/bin/git config --list --show-origin --show-scope',
       'npm ci',
       'npm run build',
