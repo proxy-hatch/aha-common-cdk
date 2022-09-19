2162bd97b61d76f0976eba101a38ad3574f44e26
Shawn Wang <shawn.wang@avancevl.com>
2022-09-19 10:55:05 +0800

    rename ssm stack and delete prop

--- src/construct/pipeline/github-ssh-private-key-ssm.ts
+++ src/construct/pipeline/github-ssh-private-key-ssm.ts
@@ -4,20 +4,15 @@ import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 import { AHA_ORGANIZATION_ACCOUNT } from '../../environment-configuration';
 import { AHA_DEFAULT_REGION } from '../../constant';
 
-
-export interface SsmStackProps {
-  readonly terminationProtection?: boolean;
-}
-
 // Retrieve the Github SSH private key from Aha Management Account
 // TODO: migrate this key to use AWS Secrets Manager, that natively support cross-account
 // https://app.zenhub.com/workspaces/back-edtech-623a878cdf3d780017775a34/issues/earnaha/api-core/1763
-export class SsmStack extends Stack {
+export class GithubSshPrivateKeySsmStack extends Stack {
   public readonly githubSshPrivateKey: string;
 
-  constructor(scope: Construct, id: string, props: SsmStackProps) {
+  constructor(scope: Construct, id: string) {
     super(scope, id, {
-      ...props, env: {
+      env: {
         account: AHA_ORGANIZATION_ACCOUNT,
         region: AHA_DEFAULT_REGION,
       },
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export * from './environment-configuration';
 export * from './construct/pipeline/pipeline-common';
 export * from './construct/pipeline/aha-pipeline';
 export * from './construct/pipeline/aha-single-env-pipeline';
-export * from './construct/pipeline/ssm';
+export * from './construct/pipeline/github-ssh-private-key-ssm';
 export * from './constant';
 export * from './util';
