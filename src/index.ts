94243eb9a3a7c62eb091717cc3a570aac232ceb3
Shawn Wang <shawn.wang@avancevl.com>
2022-09-19 10:49:37 +0800

    export ssm stack

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from './environment-configuration';
 export * from './construct/pipeline/pipeline-common';
 export * from './construct/pipeline/aha-pipeline';
 export * from './construct/pipeline/aha-single-env-pipeline';
+export * from './construct/pipeline/ssm';
 export * from './constant';
 export * from './util';
